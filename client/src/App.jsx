44c66a3d49c82f7d4d54077e08a30dbea390b9a3
Kurt Hogentogler <kurt.ah@outlook.com>
2023-09-23 12:39:58 -0600

    Added intersection observer and using useCallback to reference image element when it is the last element in the array

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 function App() {
   const [photos, setPhotos] = useState([])
+  // console.log(photos)
+
+  // When is user at very end of list of photos best way is to use a useCallback to check that.
+  // As soon as element is loaded on the page I want to run some code.
+  const imageRef = useCallback((image) => {
+    if (image == null) return
+
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        console.log(entries)
+        //TODO: Load next elements
+        console.log("Last element show")
+        observer.unobserve(image)
+      }
+    })
+    observer.observe(image)
+  }, [])
+
   useEffect(() => {
     fetch('http://localhost:3000/photos?_page=1&_limit=10')
       .then((res) => res.json())
@@ -9,9 +27,14 @@ function App() {
   }, [])
 
   return (
-    <div className='grid'>
-      {photos.map(photo => (
-        <img src={photo.url} key={photo.id} alt="" />
+    <div className="grid">
+      {photos.map((photo, index) => (
+        <img
+          src={photo.url}
+          key={photo.id}
+          ref={index === photos.length - 1 ? imageRef : undefined}
+          alt=""
+        /> // When is last image element being shown, need reference to last image element
       ))}
     </div>
   )
