5ba0ad07f7af1d078f35ebed9d3ece35332369ee
Kurt Hogentogler <kurt.ah@outlook.com>
2023-09-23 11:58:03 -0600

    implemented fetch of api and map to render photos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import { useEffect, useState } from 'react'
 
 function App() {
-  return ('Hi')
+  const [photos, setPhotos] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3000/photos?_page=1&_limit=10')
+      .then((res) => res.json())
+      .then(setPhotos)
+  }, [])
+
+  return (
+    <div className='grid'>
+      {photos.map(photo => (
+        <img src={photo.url} key={photo.id} alt="" />
+      ))}
+    </div>
+  )
 }
 
 export default App
