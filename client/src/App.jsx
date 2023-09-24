601eaf22c2876b927cb92ff322d87e6a30d8eb9c
Kurt Hogentogler <kurt.ah@outlook.com>
2023-09-24 11:12:26 -0600

    Added isLoading state to load in skeleton photos. Also implmented logic to stop isLoading skeleton photos when list of photos in api is at the end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 import { parseLinkHeader } from './parseLinkHeader'
 
+const LIMIT = 50
+const value = '_'
 function App() {
   const [photos, setPhotos] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
   const nextPhotoUrlRef = useRef()
 
   // Convert fetch to async function below
@@ -13,33 +16,36 @@ function App() {
 
   // Make above fetch with promise to async function
   async function fetchPhotos(url, { overwrite = false } = {}) {
-    const res = await fetch(url)
-    nextPhotoUrlRef.current = parseLinkHeader(res.headers.get('Link')).next
-     console.log(parseLinkHeader(res.headers.get('Link')))
-    const photos = await res.json()
-    if (overwrite) {
-      //console.log(overwrite)
-      setPhotos(photos)
-    } else {
-      setPhotos((prevPhotos) => {
-        return [...prevPhotos, ...photos]
-      })
+    setIsLoading(true)
+    try {
+      await new Promise((res) => setTimeout(res, 2000))
+      const res = await fetch(url)
+      nextPhotoUrlRef.current = parseLinkHeader(res.headers.get('Link')).next
+      // console.log(parseLinkHeader(res.headers.get('Link')))
+      const photos = await res.json()
+      if (overwrite) {
+        setPhotos(photos)
+      } else {
+        setPhotos((prevPhotos) => {
+          return [...prevPhotos, ...photos]
+        })
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setIsLoading(false)
     }
   }
-  // console.log(photos)
 
   // When is user at very end of list of photos best way is to use a useCallback to check that.
   // As soon as element is loaded on the page I want to run some code.
   const imageRef = useCallback((image) => {
-    if (image == null) return
+    if (image == null || nextPhotoUrlRef.current == null) return
 
     const observer = new IntersectionObserver((entries) => {
       if (entries[0].isIntersecting) {
-        nextPhotoUrlRef.current
-        console.log(entries)
-        //TODO: Load next elements
+        // console.log(entries)
         fetchPhotos(nextPhotoUrlRef.current)
-        console.log('Last element show')
         observer.unobserve(image) // Image shows up one time and then we stop it
       }
     })
@@ -47,21 +53,32 @@ function App() {
   }, [])
 
   useEffect(() => {
-    fetchPhotos('http://localhost:3000/photos?_page=1&_limit=10', {
-      overwrite: true,
-    })
+    fetchPhotos(
+      `http://localhost:3000/photos-short-list?_page=1&_limit=${LIMIT}`,
+      {
+        overwrite: true,
+      }
+    )
   }, [])
 
   return (
     <div className="grid">
       {photos.map((photo, index) => (
         <img
-          src={photo.url}
           key={photo.id}
+          src={photo.url}
           ref={index === photos.length - 1 ? imageRef : undefined}
           alt=""
-        /> // When is last image element being shown, need reference to last image element
+        />
       ))}
+      {isLoading &&
+        Array.from({ length: LIMIT }, (value, (index) => index)).map((n) => {
+          return (
+            <div key={n} className="skeleton">
+              Loading...
+            </div>
+          )
+        })}
     </div>
   )
 }
