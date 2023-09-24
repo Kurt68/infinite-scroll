4de7ad9327dde0f59cee5c78d84f52854db42393
Kurt Hogentogler <kurt.ah@outlook.com>
2023-09-24 12:58:40 -0600

    Final Project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,20 +8,12 @@ function App() {
   const [isLoading, setIsLoading] = useState(false)
   const nextPhotoUrlRef = useRef()
 
-  // Convert fetch to async function below
-  //   fetch('http://localhost:3000/photos?_page=1&_limit=10')
-  //     .then((res) => res.json())
-  //     .then(setPhotos)
-  // }
-
-  // Make above fetch with promise to async function
   async function fetchPhotos(url, { overwrite = false } = {}) {
     setIsLoading(true)
     try {
       await new Promise((res) => setTimeout(res, 2000))
       const res = await fetch(url)
       nextPhotoUrlRef.current = parseLinkHeader(res.headers.get('Link')).next
-      // console.log(parseLinkHeader(res.headers.get('Link')))
       const photos = await res.json()
       if (overwrite) {
         setPhotos(photos)
@@ -37,16 +29,13 @@ function App() {
     }
   }
 
-  // When is user at very end of list of photos best way is to use a useCallback to check that.
-  // As soon as element is loaded on the page I want to run some code.
   const imageRef = useCallback((image) => {
     if (image == null || nextPhotoUrlRef.current == null) return
 
     const observer = new IntersectionObserver((entries) => {
       if (entries[0].isIntersecting) {
-        // console.log(entries)
         fetchPhotos(nextPhotoUrlRef.current)
-        observer.unobserve(image) // Image shows up one time and then we stop it
+        observer.unobserve(image)
       }
     })
     observer.observe(image)
